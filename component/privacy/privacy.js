108bbb4e8ef0fa02f07f7b58fc32eeda3cdde901
Jiaqi <s@94xy.com>
2023-08-31 15:45:27 +0800

    兼容基础库2.32.3以下

--- component/privacy/privacy.js
+++ component/privacy/privacy.js
@@ -13,16 +13,19 @@ Component({
     pageLifetimes: {
         show() {
             const _ = this
-            wx.getPrivacySetting({
-                success(res) {
-                    if (res.errMsg == "getPrivacySetting:ok") {
-                        _.setData({
-                            privacyContractName: res.privacyContractName,
-                            showPrivacy: res.needAuthorization
-                        })
+            const version = wx.getAppBaseInfo().SDKVersion
+            if (_.compareVersion(version, '2.32.3') >= 0) {
+                wx.getPrivacySetting({
+                    success(res) {
+                        if (res.errMsg == "getPrivacySetting:ok") {
+                            _.setData({
+                                privacyContractName: res.privacyContractName,
+                                showPrivacy: res.needAuthorization
+                            })
+                        }
                     }
-                }
-            })
+                })
+            }
         }
     },
     /**
@@ -53,5 +56,31 @@ Component({
                 showPrivacy: false
             })
         },
+        // 比较版本号
+        compareVersion(v1, v2) {
+            v1 = v1.split('.')
+            v2 = v2.split('.')
+            const len = Math.max(v1.length, v2.length)
+
+            while (v1.length < len) {
+                v1.push('0')
+            }
+            while (v2.length < len) {
+                v2.push('0')
+            }
+
+            for (let i = 0; i < len; i++) {
+                const num1 = parseInt(v1[i])
+                const num2 = parseInt(v2[i])
+
+                if (num1 > num2) {
+                    return 1
+                } else if (num1 < num2) {
+                    return -1
+                }
+            }
+
+            return 0
+        }
     },
 })
